4c17546575b1c2892b37eaa98b16bf11cae81b21
SergioCamarena <sergio.camarena@opendeusto.es>
2021-11-14 13:32:43 +0100

    Creación ventana stats y tabla para los rankings
    queda por arreglar la tabla de los rankings para que se vea bien en la ventana

--- CookieClicker_proyecto_prog3/src/Code/VentanaOptions.java
+++ CookieClicker_proyecto_prog3/src/Code/VentanaOptions.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
--- CookieClicker_proyecto_prog3/src/Code/VentanaStats.java
+++ CookieClicker_proyecto_prog3/src/Code/VentanaStats.java
@@ -1,8 +1,79 @@
 package Code;
 
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 public class VentanaStats {
 
 	public VentanaStats() {
+		JFrame ventanaS = new JFrame("ESTADISTICAS DE: "); //Anyadir el nombre del usuario en específico que esté consultando las estadísticas  
+		JPanel norte = new JPanel();
+		JLabel titulo = new JLabel("ESTADISTICAS DE: ");
+		titulo.setFont(new Font("Serif", Font.PLAIN,40 ));
+		norte.add(titulo);
+		ventanaS.add(norte, BorderLayout.NORTH );
 		
+		JPanel oeste = new JPanel();
+		oeste.setLayout(new GridLayout(7,2));
+		
+		JLabel cookies_ps = new JLabel("Cookies per second ");
+		JLabel psvariable = new JLabel();
+		oeste.add(cookies_ps);
+		oeste.add(psvariable);
+		
+		JLabel totalBuildings = new JLabel("Numero total de edificios adquiridos: ");
+		JLabel buildingsVar = new JLabel();
+		oeste.add(totalBuildings);
+		oeste.add(buildingsVar);
+		
+		JLabel totalcookies = new JLabel("Cookies totales cocinadas: ");
+		JLabel totalCookieVar = new JLabel();
+		oeste.add(totalcookies);
+		oeste.add(totalCookieVar);
+		
+		JLabel tTotalJuego = new JLabel("Tu legado empezo hace: ");
+		JLabel tTotalJuegoVar = new JLabel();
+		oeste.add(tTotalJuego);
+		oeste.add(tTotalJuegoVar);
+		
+		JLabel tPartida = new JLabel("LLevas cocinando cookies sin parar: ");
+		JLabel tPartidaVar = new JLabel();
+		oeste.add(tPartida);
+		oeste.add(tPartidaVar);
+		
+		ventanaS.add(oeste, BorderLayout.WEST );
+		
+		JPanel sur = new JPanel();
+		sur.setLayout(new GridLayout(1,1));
+		JButton rankings = new JButton("Rankings");
+		sur.add(rankings);
+		ventanaS.add(sur, BorderLayout.SOUTH);
+		rankings.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				new Tabla();
+				
+			}
+		});
+		
+		
+		
+		ventanaS.setBounds(300, 0, 800, 400);
+		ventanaS.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		ventanaS.setVisible(true);
+	}
+	public static void main (String[]args) {
+		new VentanaStats();
 	}
 }
