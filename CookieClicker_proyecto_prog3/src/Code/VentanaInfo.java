f51572b6b0cf1fe05e874c3cf96fd723a41f1502
elaiartabe <elaia.artabe@opendeusto.es>
2021-11-14 19:45:21 +0100

    intento de scroll

--- CookieClicker_proyecto_prog3/src/Code/VentanaInfo.java
+++ CookieClicker_proyecto_prog3/src/Code/VentanaInfo.java
@@ -2,18 +2,29 @@ package Code;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.ScrollPane;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
 
 public class VentanaInfo {
 	
 	public VentanaInfo() {
 		JFrame ventanaI = new JFrame();
+		JTextArea txArea1 = new JTextArea();
+		JLabel texto = new JLabel();
 		ventanaI.setBounds(0, 0, 600, 400);
 		ventanaI.setLayout(new BorderLayout());
+		
+		//Scroll
+		JScrollPane scrollBar = new JScrollPane(txArea1);
+		scrollBar.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		texto.add(scrollBar, BorderLayout.SOUTH);
 		
 		//Panel norte
 		JPanel norte =  new JPanel();
@@ -29,10 +40,10 @@ public class VentanaInfo {
 		JPanel centro = new JPanel();
 		JLabel versiones = new JLabel("<html>Hola compañeros</html>");
 		centro.add(versiones);
-		ventanaI.add(centro, BorderLayout.CENTER);
-		
+//		ventanaI.add(centro, BorderLayout.CENTER);
 		ventanaI.setVisible(true);
 		ventanaI.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		ventanaI.add(new JScrollPane(centro), BorderLayout.CENTER);
 	}
 	public static void main(String[] args) {
 		new VentanaInfo();
