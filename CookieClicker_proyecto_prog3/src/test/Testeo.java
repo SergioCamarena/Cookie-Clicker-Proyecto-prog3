5e977dfe8dea15e0ce806a584682db0f26668fc4
elaiartabe <elaia.artabe@opendeusto.es>
2021-12-08 17:27:27 +0100

    Cosas sin acabar
    La mayor parte de las cosas estan comentada el codigo esta bien pero necesito añadir algunas cosas o en algunos sitios se debe de solucionar algo

--- CookieClicker_proyecto_prog3/src/test/Testeo.java
+++ CookieClicker_proyecto_prog3/src/test/Testeo.java
@@ -1,5 +1,25 @@
 package test;
 
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import gestion.de.datos.BaseDeDatos;
+
 public class Testeo {
+	//NECESITO QUE LA BASE DE DATOS ESTE CORRECTAMENTE HECHA
+	
+	//test para inserta un nuevos datos.
+//	@Test
+//	public void InsertarTest() {
+//		try {
+//			BaseDeDatos.open("Ranking", false);
+//			
+//		}catch (SQLException e){
+//			e.printStackTrace();
+//		}
+//	}
+	//test para saber si se borra correctamente un usuario
+	
 
 }
