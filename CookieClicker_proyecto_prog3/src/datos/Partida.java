5e21e974236ee289b2c33d3c45f178984f189312
SergioCamarena <sergio.camarena@opendeusto.es>
2021-12-23 09:43:21 +0100

    Ultimos cambios BD

--- CookieClicker_proyecto_prog3/src/datos/Partida.java
+++ CookieClicker_proyecto_prog3/src/datos/Partida.java
@@ -7,10 +7,21 @@ public class Partida {
 	public int cookie_ps;
 	public int edif_tot;
 	public int tiempo_tot;
+	public int grandmas;
+	public int farms;
+	public int mine;
+	public int factory;
+	public int bank;
+	public int temple;
+	public int w_tower;
+	public int shipment;
 	
 	
 	
-	public Partida(int cod_partida, String nom_usuario, int cookie_tot, int cookie_ps, int edif_tot, int tiempo_tot) {
+
+	
+	public Partida(int cod_partida, String nom_usuario, int cookie_tot, int cookie_ps, int edif_tot, int tiempo_tot,
+			int grandmas, int farms, int mine, int factory, int bank, int temple, int w_tower, int shipment) {
 		super();
 		if(nom_usuario == null){
 			throw new NullPointerException("Nombre de usuario es nulo");
@@ -21,6 +32,14 @@ public class Partida {
 		this.cookie_ps = cookie_ps;
 		this.edif_tot = edif_tot;
 		this.tiempo_tot = tiempo_tot;
+		this.grandmas = grandmas;
+		this.farms = farms;
+		this.mine = mine;
+		this.factory = factory;
+		this.bank = bank;
+		this.temple = temple;
+		this.w_tower = w_tower;
+		this.shipment = shipment;
 	}
 	//Getters y setters
 	public int getCod_partida() {
@@ -52,6 +71,54 @@ public class Partida {
 	}
 	public void setTiempo_tot(int tiempo_tot) {
 		this.tiempo_tot = tiempo_tot;
+	}
+	public int getGrandmas() {
+		return grandmas;
+	}
+	public void setGrandmas(int grandmas) {
+		this.grandmas = grandmas;
+	}
+	public int getFarms() {
+		return farms;
+	}
+	public void setFarms(int farms) {
+		this.farms = farms;
+	}
+	public int getMine() {
+		return mine;
+	}
+	public void setMine(int mine) {
+		this.mine = mine;
+	}
+	public int getFactory() {
+		return factory;
+	}
+	public void setFactory(int factory) {
+		this.factory = factory;
+	}
+	public int getBank() {
+		return bank;
+	}
+	public void setBank(int bank) {
+		this.bank = bank;
+	}
+	public int getTemple() {
+		return temple;
+	}
+	public void setTemple(int temple) {
+		this.temple = temple;
+	}
+	public int getW_tower() {
+		return w_tower;
+	}
+	public void setW_tower(int w_tower) {
+		this.w_tower = w_tower;
+	}
+	public int getShipment() {
+		return shipment;
+	}
+	public void setShipment(int shipment) {
+		this.shipment = shipment;
 	}
 	
 	
