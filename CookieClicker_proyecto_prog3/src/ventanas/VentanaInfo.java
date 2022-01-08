9e43ab0b29b9697e4087b8c44548167497fad020
elaiartabe <elaia.artabe@opendeusto.es>
2022-01-08 09:33:30 +0100

    VentanaInfo

--- CookieClicker_proyecto_prog3/src/ventanas/VentanaInfo.java
+++ CookieClicker_proyecto_prog3/src/ventanas/VentanaInfo.java
@@ -29,13 +29,9 @@ import javax.swing.ScrollPaneConstants;
 
 public class VentanaInfo extends JFrame{
 	private JTextArea informacion;
-	//private static JFrame ventanaI;
-	JScrollPane spTexto;  // atributo de paquete para poder hacer test de JUnit
+	JScrollPane spTexto; 
 	
 	public VentanaInfo() {
-		//ventanaI = new JFrame();
-		//ventanaI.setBounds(0, 0, 600, 400);
-		//ventanaI.setLayout(new BorderLayout());
 		setBounds(0,0,600,400);
 		setLayout(new BorderLayout());
 		
@@ -46,33 +42,13 @@ public class VentanaInfo extends JFrame{
 		norte.add(titulo);
 		
 		add(norte, BorderLayout.NORTH);
-		//ventanaI.add(norte, BorderLayout.NORTH);
 
 		
-		//Panel centro
-		//JPanel centro = new JPanel();
+		
 		informacion = new JTextArea();
-		//centro.add(informacion);
 		spTexto = new JScrollPane(informacion);
-		//ventanaI.add(informacion, BorderLayout.CENTER);
-		//ventanaI.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		add(informacion, BorderLayout.CENTER);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		//DA ERROR EN EL VOID PERO NO ENTIENDO EL PORQUE PERO EL CODIGO DEBE SER ASI
-		
-//		protected void cargaInformacion() {
-//			try {
-//				Scanner scanner = new Scanner(VentanaInfo.class.getResourceAsStream("Informacion.txt"), "UTF-8");
-//				while(scanner.hasNextLine()) {
-//					String linea = scanner.nextLine();
-//					informacion.append(linea + "\n");
-//				}
-//				scanner.close();
-//			}catch (Exception e) {
-//				JOptionPane.showInputDialog(this, "No se ha podido cargar el texto");
-//			}
-//		}
-
 	
 	//Boton para volver a la ventana cookie
 		JPanel panel = new JPanel(new FlowLayout(FlowLayout.TRAILING));
@@ -80,25 +56,24 @@ public class VentanaInfo extends JFrame{
 		panel.add(cookie);
 		//ventanaI.getContentPane().add(panel,BorderLayout.EAST);
 		getContentPane().add(panel, BorderLayout.EAST);
-//		//Se llama a la ventana de cookie
+		//Se llama a la ventana de cookie
 		cookie.addActionListener(new ActionListener() {
 		
 		@Override
 			public void actionPerformed(ActionEvent e) {
 				//new VentanaCookie(); //Esto no hay que hacer porque se crea otra nueva entonces vuelve todo a empezar otra vez
 				dispose();
-			//ventanaI.dispose();
+				//ventanaI.dispose();
 			
 
 			}
 		});
-		//ventanaI.setVisible(true);
 	}
 	
-	//HAY QUE VER PORQUE NO FUNCIONA
+	//Metodo para cargar la informacion desde un fichero de texto
 	protected void cargaInformacion() {
 		try {
-			Scanner scanner = new Scanner(VentanaInfo.class.getResourceAsStream("/CookieClicker_proyecto_prog3/src/Informacion.txt"), "UTF-8");
+			Scanner scanner = new Scanner(new File("src/Informacion.txt"));
 			while(scanner.hasNextLine()) {
 				String linea = scanner.nextLine();
 				informacion.append(linea + "\n");
@@ -106,31 +81,14 @@ public class VentanaInfo extends JFrame{
 			scanner.close();
 		}catch (Exception e) {
 			JOptionPane.showInputDialog(this, "No se ha podido cargar el texto");
-			//e.printStackTrace();
 		}
 
 	}
 
 	public static void main(String[] args){
-		//ventanaI.cargaInformacion();
 		VentanaInfo ventanaI = new VentanaInfo();
-		//ventanaI.cargaInformacion();
 		ventanaI.setVisible(true);
 		ventanaI.cargaInformacion();
-		//new VentanaInfo();
-		
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					VentanaInfo window = new VentanaInfo();
-//					//window.cargaInformacion();
-//					window.ventanaI.setVisible(true);
-//					window.cargaInformacion();
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
 
 
 	}
